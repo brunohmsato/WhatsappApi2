314f9298526ef574fae270d30dcca676a167cb41
Bruno H. M. Sato <bruno.sato@fenoxtec.com.br>
2025-02-05 13:06:22 -0300

    ajustes;

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(express.json());
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: { 
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || require("puppeteer").executablePath(),
     headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"]
    }
